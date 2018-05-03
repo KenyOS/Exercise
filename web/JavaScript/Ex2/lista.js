f245d2affb789cd3f46d5398d26e60fa22edfb81
Keny Oshiro <keny.oshiro92@gmail.com>
2018-05-03 20:57:36 -0300

    Create lista.js

---
+++ web/JavaScript/Ex2/lista.js
@@ -0,0 +1,8 @@
+function calculadora() {
+	var v1 = document.demerval.valor1.value;
+	var v1 = document.demerval.valor1.value;
+	var op = document.demerval.operacao.value;
+	
+	//debugar o js
+	console.log(v1+op+v2);
+}
