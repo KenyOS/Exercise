14a15d968e19b02dc70f754e7de616f86bdc4292
Keny Oshiro <keny.oshiro92@gmail.com>
2019-04-30 22:31:35 -0300

    Update temp_return_func.c

--- 3SEM/Language C/temp_return_func.c
+++ 3SEM/Language C/temp_return_func.c
@@ -18,7 +18,15 @@ int le_temp(float temp[N]) {
 	
 void Max_Min(float temp[N], int n, float *maxima, float *minima)
 {
-	for()
+	int i;
+	*maxima = temp[0];
+	*minima = *maxima;
+	for(i=1;i<n;i++) {
+		if (temp[i] > *maxima)
+			*maxima = temp[i];
+		if (temp[i] < *minima)
+			*minima = temp[i];
+	}
 }
 
 int main() {
