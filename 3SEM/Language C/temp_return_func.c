6a7037ba6978a61ede5ea67a7f68dddc1c9b856a
Keny Oshiro <keny.oshiro92@gmail.com>
2019-04-30 22:24:12 -0300

    Create temp_return_func.c

---
+++ 3SEM/Language C/temp_return_func.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 30
+
+int le_temp(float temp[N]) {
+	int i=0, resp;
+	do {
+	printf("Digite a Temperatura: ");
+	scanf("%f", &temp[i);
+	i++;
+	printf("Digite 1 para continuar");
+	scanf("%d", &resp);	
+	}while(i<N && resp==1)
+	return(i)
+}
+	
+void Max_Min(float temp[N], int n, float *maxima, float *minima)
+{
+	for()
+}
+
+int main() {
+	float temp[N];
+	int n;
+	float max, min;
+	n = le_temp(temp);
+	
+	
+}
